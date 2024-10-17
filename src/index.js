0e68cfe14bbb598a94c48f8720f217d188ab3697
oweeeenie <owensullivan419@yahoo.com>
2024-10-16 21:42:50 -0400

    made image folder, title, and a bit more! not enough time today to do a lot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import "./styles.css";
+import './styles.css';
+
+const content = document.querySelector('#content');
+
+const title = document.createElement('h1');
+title.className = 'title';
+title.textContent = "Owen's Scrumptious Bar!";
+
+content.appendChild(title);
