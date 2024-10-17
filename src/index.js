4618165d51b87465c95a2240763d3eb21c6e6eab
oweeeenie <owensullivan419@yahoo.com>
2024-10-17 13:43:18 -0400

    nav is finished for now, functionality later

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import './styles.css';
 
 const content = document.querySelector('#content');
 
+const nav = document.querySelector('.nav');
+const navContainer = document.createElement('div');
+navContainer.className = 'nav-container';
+const tabs = document.querySelectorAll('.tabs');
+
 const title = document.createElement('h1');
 title.className = 'title';
-title.textContent = "Owen's Scrumptious Bar!";
+title.textContent = "Owen's Scrumptious Bar";
 
-content.appendChild(title);
+nav.appendChild(title);
+
+navContainer.appendChild(tabs[0]);
+navContainer.appendChild(tabs[1]);
+navContainer.appendChild(tabs[2]);
+nav.appendChild(navContainer);
