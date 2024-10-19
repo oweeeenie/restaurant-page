96cbc2f17ea23dafa33fa4ee8fbd951d365a1566
oweeeenie <owensullivan419@yahoo.com>
2024-10-19 19:57:11 -0400

    just add more to the menu lists then we're done my brotha

--- src/menu.js
+++ src/menu.js
@@ -35,4 +35,58 @@ export function loadMenuTab() {
   const clippedContainerDeserts = document.createElement('div');
   clippedContainerDeserts.className = 'clipped-container-deserts';
   menuContent.appendChild(clippedContainerDeserts);
+
+  const foodItems = {
+    entrees: [
+      {
+        name: 'Steak',
+        description: 'Juicy and tender, cooked just how you need it to be.',
+      },
+      {
+        name: 'Chicken',
+        description:
+          'Fresh breasts, thighs, or legs cooked to a delicious amount of crisp.',
+      },
+      {
+        name: 'Pasta',
+        description: 'Freshly made pasta in a rich marinara sauce.',
+      },
+    ],
+    drinks: [
+      {
+        name: 'Red Wine',
+        description: 'Rich flavor, full bodied wine.',
+      },
+      {
+        name: 'Water',
+        description: 'Our purified water only as fresh as they come.',
+      },
+    ],
+    deserts: [
+      {
+        name: 'Cheesecake',
+        description: 'Rich and creamy cheesecake.',
+      },
+      {
+        name: 'Brownies',
+        description: 'Baked straight out of the oven and soft.',
+      },
+    ],
+  };
+  appendFoodItems(clippedContainerEntrees, foodItems.entrees);
+  appendFoodItems(clippedContainerDrinks, foodItems.drinks);
+  appendFoodItems(clippedContainerDeserts, foodItems.deserts);
+}
+
+function appendFoodItems(container, items) {
+  items.forEach((item) => {
+    const itemHeader = document.createElement('h2');
+    itemHeader.textContent = item.name;
+
+    const itemDescription = document.createElement('p');
+    itemDescription.textContent = item.description;
+
+    container.appendChild(itemHeader);
+    container.appendChild(itemDescription);
+  });
 }
