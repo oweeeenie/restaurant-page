2342ef3f76094bb6b07301ed7a032323a33cdafe
oweeeenie <owensullivan419@yahoo.com>
2024-10-21 13:27:30 -0400

    home functionality works, just need to clean up code + new css files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import './styles.css';
 import { loadAboutTab } from './about';
 import { loadMenuTab } from './menu';
 
-const content = document.querySelector('#content'); // holds EVERYTHING
-
 const nav = document.querySelector('.nav'); // holds nav buttons & title
 const navContainer = document.createElement('div'); // only holds nav buttons
 navContainer.className = 'nav-container';
@@ -19,23 +17,28 @@ navContainer.appendChild(tabs[1]);
 navContainer.appendChild(tabs[2]);
 nav.appendChild(navContainer);
 
-const quoteContainer = document.createElement('div');
-quoteContainer.className = 'quote-container';
-content.appendChild(quoteContainer);
+function loadHomeTab() {
+  const content = document.querySelector('#content'); // holds EVERYTHING
+  content.innerHTML = '';
 
-const quote = document.createElement('div');
-quote.className = 'quote';
-quote.textContent = 'A Premium And Rewarding Restaurant';
-quoteContainer.appendChild(quote);
+  const quoteContainer = document.createElement('div');
+  quoteContainer.className = 'quote-container';
+  content.appendChild(quoteContainer);
 
-const quoteButton = document.createElement('button');
-quoteButton.className = 'quote-button';
-quoteButton.textContent = 'Book A Table';
-quoteContainer.appendChild(quoteButton);
+  const quote = document.createElement('div');
+  quote.className = 'quote';
+  quote.textContent = 'A Premium And Rewarding Restaurant';
+  quoteContainer.appendChild(quote);
 
-const footer = document.createElement('footer');
-footer.textContent = '© 2024 Owen’s Scrumptious Bar';
-document.body.appendChild(footer);
+  const quoteButton = document.createElement('button');
+  quoteButton.className = 'quote-button';
+  quoteButton.textContent = 'Book A Table';
+  quoteContainer.appendChild(quoteButton);
+
+  const footer = document.createElement('footer');
+  footer.textContent = '© 2024 Owen’s Scrumptious Bar';
+  document.body.appendChild(footer);
+}
 
 // START OF OTHER TABS ------------------------------------
 
@@ -53,4 +56,14 @@ const menuButton = document.querySelector('.tabs:nth-child(3)');
 menuButton.addEventListener('click', () => {
   clearContent();
   loadMenuTab();
+});
+
+const homeButton = document.querySelector('.tabs:nth-child(1)');
+homeButton.addEventListener('click', () => {
+  clearContent();
+  loadHomeTab();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadHomeTab();
 });
